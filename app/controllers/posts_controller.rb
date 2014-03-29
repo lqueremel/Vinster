ff664483d6c5a532fc72cf0d5cd6355d93155e0a
Luis Queremel <lqueremel@gmail.com>
2014-03-29 21:24:38 +0000

    Update 9 Final Saturday

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 	def index
 		#Everything here will get run when someone goes to /posts
 		@posts = Post.all
-		@body_class = "post_pages"
+		@body_class = "posts_pages"
 	end
 	
 
