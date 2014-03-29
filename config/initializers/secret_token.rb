b3dcc13115980d52aadec80345fb4757fe278f55
Luis Queremel <lqueremel@gmail.com>
2014-03-29 17:34:35 +0000

    Backup One

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vinster::Application.config.secret_key_base = '2863b8729000c931f801dd77ab280bd7abe1e2b03c0ddbd11175bc040a4fc67c774e8cae704cb466612792d903c99a6f6990d1493a70f7099914ab2a5d7069ea'
