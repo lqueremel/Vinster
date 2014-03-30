9c46057c57fdd4f14d156cb839c7bb7a42d86e88
Luis Queremel <lqueremel@gmail.com>
2014-03-30 16:40:48 +0000

    Update 12 Images on site

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
 
 	private
 	def post_params
-		params.require(:post).permit(:title, :description)
+		params.require(:post).permit(:title, :description, :image)
 	
 	end
 
